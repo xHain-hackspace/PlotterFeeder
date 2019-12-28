78fe93157b3d082a1a4c642f4b84806cd6719e41
arglurgl <arglurgl@github.com>
2019-12-28 21:49:59 +0100

    Switch to pygame for joystick handling

--- src/remotecontrol_wifi.py
+++ src/remotecontrol_wifi.py
@@ -1,13 +1,18 @@
 #this script is written for python3
-from inputs import devices
+#from inputs import devices
 import time
-from inputs import get_gamepad
+#from inputs import get_gamepad
+import pygame
 import socket               
 
 WIFI_INPUT_BUFFER_SIZE_ESP32 = 255 #must be the same as in esp32 plotterfeeder code
 
-UPDATE_DELAY =0.050 #seconds
+SEND_DELAY =0.050 #seconds, for sending position to plotter
+JOYSTICK_DELAY = 0.005 #seconds, for handling jostick changes
+MAX_SPEED = 7500
 
+JOYSTICK_DEADZONE = 0.05 #+/- this value around the events = get_gamepad()middle position is ignored
+JOYSTICK_MIDDLE_VALUE = 0
 
 #helper function to split string in chunks
 def chunks(lst, n):
@@ -35,6 +40,10 @@ def send_absolute_position():
 
 
 ###start main code###
+pygame.init()
+pygame.joystick.init()
+myjoystick = pygame.joystick.Joystick(0)
+myjoystick.init()
 
 #connect to plotter
 sock = socket.socket() 
@@ -42,31 +51,43 @@ host = "192.168.4.1" #Plotter IP in local network
 port = 1337           #Plotter Server Port     
 sock.connect((host, port))#connect to plotter, TODO: this hangs on "no reply"
 
-#get pen
-send_plotter("SP2;")
+last_joystick_poll_time = time.time()
+last_button_status = 0
 
 while 1:
-    events = get_gamepad()
-    for event in events:#handle gamepad events, we only get an event for changed values
-        if (event.code == "ABS_X"):
-            #print(event.ev_type, event.code, event.state)
-            current_x_pos =event.state * 40
-        if (event.code == "ABS_Y"):
-            #print(event.ev_type, event.code, event.state)
-            current_y_pos = (255-event.state) *30
-        if (event.code == "BTN_THUMB2"):
-            #print(event.ev_type, event.code, event.state)
-            button_status =event.state
-            if (event.state == 1):
-                print("Button down")
-                send_plotter("PD;")
-            if (event.state == 0):
-                print("Button up") 
-                send_plotter("PU;")       
+    #get joystick values
+    pygame.event.pump()
+    joystick_x = myjoystick.get_axis(0)
+    joystick_y = myjoystick.get_axis(1)
+    button_status = myjoystick.get_button(2)
 
-    if((time.time()-last_position_update_time)>=UPDATE_DELAY):
+    #update local absolute position based on current joystick position
+    if((time.time()-last_joystick_poll_time)>=JOYSTICK_DELAY):
+        if ((joystick_y < (JOYSTICK_MIDDLE_VALUE-JOYSTICK_DEADZONE)) or (joystick_y > (JOYSTICK_MIDDLE_VALUE+JOYSTICK_DEADZONE))):
+            current_y_pos += joystick_y*-1*MAX_SPEED*JOYSTICK_DELAY
+            if current_y_pos <0:
+                current_y_pos=0
+            if current_y_pos > 7500:
+                current_y_pos = 7500
+        if ((joystick_x < (JOYSTICK_MIDDLE_VALUE-JOYSTICK_DEADZONE)) or (joystick_x > (JOYSTICK_MIDDLE_VALUE+JOYSTICK_DEADZONE))):
+                current_x_pos += joystick_x*MAX_SPEED*JOYSTICK_DELAY
+                if current_x_pos <0:
+                    current_x_pos=0
+                if current_x_pos > 10700:
+                    current_x_pos = 10700
+        last_joystick_poll_time = time.time()
+
+    #send updated position to plotter and also check for button press for pen           
+    if((time.time()-last_position_update_time)>=SEND_DELAY):
         send_absolute_position()
+        #check for button press
+        if (button_status != last_button_status):
+            last_button_status = button_status
+            if(button_status == 1): send_plotter("PD;")                            
+            else: send_plotter("PU;")
         last_position_update_time = time.time()
+        
+        
 
 sock.close()
 print("Done.")
