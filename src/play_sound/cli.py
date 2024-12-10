db9d8f5c0660e1758eb99c9067af7b19bbabb818
ranomier <ranomier@fragomat.net>
2024-12-10 02:12:38 +0100

    added midi parameter

--- src/play_sound/cli.py
+++ src/play_sound/cli.py
@@ -1,19 +1,37 @@
 #!/usr/bin/env python
 from play_sound import *
 from melodies_twisst import *
+from melodies_midi import *
 import argparse
 import sys
+from os import path
 
 
+
+def check_midi_param(values):
+    if not path.isfile(values[0]):
+        print("Please enter path to file")
+        sys.exit(1)
+    try:
+        values[1] = int(values[1])
+    except ValueError:
+        print("Please enter integer")
+        sys.exit(1)
+    return values
 
 def main():
 
     ap = argparse.ArgumentParser(prog="PlotterMusic", description="Play music on the PlotterFeeder")
     ap.add_argument("--id", type=int, help="play song with specific id")
+    ap.add_argument("--midi", nargs=2, help="play midi file")
     args = ap.parse_args()
+
 
     if args.id:
         song = get_song_twisst(args.id)
+    elif args.midi:
+        check_midi_param(args.midi)
+        song = get_song_midi(args.midi[0], args.midi[1])
     else:
         sys.exit(0)
 
